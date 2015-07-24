15240bb15ccf9d76edfd96f7b51e922cac685f1f
Jan Manaloto <janmanaloto@gmail.com>
2015-07-24 21:20:36 +0800

    added demos for talk

--- routes/demos/index.js
+++ routes/demos/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var request = require('request');
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.post('/', function(req, res) {
 
  res.setHeader('Access-Control-Allow-Origin', '*');
 
@@ -17,11 +17,12 @@ router.get('/', function(req, res) {
     // to the API (e.g. in case you use sessions)
     var key = req.body.key;
     var msg = req.body.msg;
-
+console.log(key); console.log(msg);
 
 	var url = "http://sandbox.api.simsimi.com/request.p?key="+key+"&lc=en&ft=1.0&text="+msg;
 	request(url, function (error, response, body) {
 	    if (!error && response.statusCode == 200) {
+	    	console.log(body);
 	        res.send(body);
 	    }
 	});
