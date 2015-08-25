6c389bef286264fa63efee095e90ed8abc548438
Jan Manaloto <janmanaloto@gmail.com>
2015-08-25 21:24:03 +0800

    splash

--- routes/Home/index.js
+++ routes/Home/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var LoginServices = require('../../services/LoginServices.js');
 
 /* GET home page. */
-router.get('/', LoginServices.requireLogin, function(req, res) {
+router.get('/', function(req, res) {
 	// res.render("Home/index.jade", { "sessionUsername": req.session.user.username });
 	   res.render("Home/index.html");
 });
