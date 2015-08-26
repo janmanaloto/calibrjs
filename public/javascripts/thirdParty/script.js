8dd9ff84db6d4502643892b91d9db70528539fee
Jan Manaloto <janmanaloto@gmail.com>
2015-08-26 20:07:21 +0800

    splash

--- public/javascripts/thirdParty/script.js
+++ public/javascripts/thirdParty/script.js
@@ -4,5 +4,25 @@ $(document).ready(function() {
         strings: ["...", "ASAWA NI BABALU"],
         typeSpeed: 200
       });
+
+//     var tl = new TimelineMax({
+//   repeat: -1
+// });
+
+// tl.add(
+//   TweenMax.from(".logo-svg", 2, {
+//     scale: 0.5,
+//     rotation: 360,
+//     ease: Elastic.easeInOut
+//   })
+// );
+
+// tl.add(
+//   TweenMax.to(".logo-svg", 4, {
+//     scale: 0.5,
+//     rotation: 360,
+//     ease: Elastic.easeInOut
+//   })
+// );
     
-});
+ });
